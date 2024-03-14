8de2913f08b775fb90aabc616732ba615c782d81
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-03-14 20:15:26 +0600

    Added a route that echoes responses to GET requests

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+
 const port = 8000;
+const localhost = `http://localhost:${port}`;
+
+const Vigenere = require('caesar-salad').Vigenere;
+const password = "LettersOnly";
+
 
 app.get('/', (req, res) => {
-    res.send("Hello, World")
+  res.send("<h1>Hello, world!!</h1>");
+});
+
+app.get('/:message', (req, res) => {
+  res.send(`<h1>Hello, ${req.params.message}!!</h1>`);
 });
 
 app.listen(port, () => {
-   console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at ${localhost}`);
 });
