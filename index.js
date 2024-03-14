0cff90c0dad517dd8415bc1bd41d6a1ff05fe122
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-03-14 20:21:32 +0600

    Added routes encode and decode for encoding and decoding the route name

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const localhost = `http://localhost:${port}`;
 const Vigenere = require('caesar-salad').Vigenere;
 const password = "LettersOnly";
 
+const createHtmlElement = (event, route) => {
+  return `<div>
+            <h1>${event}</h1>
+            <button type="button">
+              <a href="${localhost}/${route}/${event}">
+                ${route.toUpperCase()}
+              </a>
+            </button>
+       </div>`
+};
 
 app.get('/', (req, res) => {
   res.send("<h1>Hello, world!!</h1>");
@@ -15,6 +25,17 @@ app.get('/', (req, res) => {
 app.get('/:message', (req, res) => {
   res.send(`<h1>Hello, ${req.params.message}!!</h1>`);
 });
+
+app.get('/encode/:route', (req, res) => {
+  const encodedRoute = Vigenere.Cipher(password).crypt(req.params.route);
+  res.send(createHtmlElement(encodedRoute, "decode"));
+});
+
+app.get('/decode/:route', (req, res) => {
+  const decodedRoute = Vigenere.Decipher(password).crypt(req.params.route);
+  res.send(createHtmlElement(decodedRoute, "encode"));
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at ${localhost}`);
